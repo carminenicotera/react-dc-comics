92b4e3960ef86188efc27ca383fa95544138336e
carminenicotera <carmine.91.n@hotmail.it>
2026-03-10 12:26:22 +0100

    update footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,42 @@
 import dcLogoFooter from '/public/img/dc-logo-bg.png'
 
+const dcComicsLinks = [
+  { id: 1, title: "Characters", href: "#" },
+  { id: 2, title: "Comics", href: "#" },
+  { id: 3, title: "Movies", href: "#" },
+  { id: 4, title: "TV", href: "#" },
+  { id: 5, title: "Games", href: "#" },
+  { id: 6, title: "Videos", href: "#" },
+  { id: 7, title: "News", href: "#" }
+];
+
+const shopLinks = [
+  { id: 1, title: "Shop DC", href: "#" },
+  { id: 2, title: "Shop DC Collectibles", href: "#" }
+];
+
+const dcLinks = [
+  { id: 1, title: "Terms Of Use", href: "#" },
+  { id: 2, title: "Privacy Policy (New)", href: "#" },
+  { id: 3, title: "Ad Choices", href: "#" },
+  { id: 4, title: "Advertising", href: "#" },
+  { id: 5, title: "Jobs", href: "#" },
+  { id: 6, title: "Subscriptions", href: "#" },
+  { id: 7, title: "Talent Workshops", href: "#" },
+  { id: 8, title: "CPSC Certificates", href: "#" },
+  { id: 9, title: "Ratings", href: "#" },
+  { id: 10, title: "Shop Help", href: "#" },
+  { id: 11, title: "Contact Us", href: "#" }
+];
+
+const sitesLinks = [
+  { id: 1, title: "DC", href: "#" },
+  { id: 2, title: "MAD Magazine", href: "#" },
+  { id: 3, title: "DC Kids", href: "#" },
+  { id: 4, title: "DC Universe", href: "#" },
+  { id: 5, title: "DC Power Visa", href: "#" }
+];
+
 export default function AppFooter() {
 
   return (
@@ -11,103 +48,64 @@ export default function AppFooter() {
             <div className="col">
               <h3>DC COMICS</h3>
               <ul className="list-unstyled">
-                <li>
-                  <a href="#" className='nav-link'>Characters</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Comics</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Movies</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>TV</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Games</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Videos</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>News</a>
-                </li>
+
+                { dcComicsLinks.map(link => (
+                  <li key={ link.id }>
+                    <a href={ link.href } className="nav-link">
+                      { link.title }
+                    </a>
+                  </li>
+                )) }
+
               </ul>
 
               <h3>SHOP</h3>
               <ul className="list-unstyled">
-                <li>
-                  <a href="#" className='nav-link'>Shop DC</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Shop DC Collectibles</a>
-                </li>
+
+                { shopLinks.map(link => (
+                  <li key={ link.id }>
+                    <a href={ link.href } className="nav-link">
+                      { link.title }
+                    </a>
+                  </li>
+                )) }
+
               </ul>
             </div>
 
             <div className="col">
               <h3>DC</h3>
               <ul className="list-unstyled">
-                <li>
-                  <a href="#" className='nav-link'>Terms Of Use</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Privacy Policy (New)</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Ad Choices</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Advertising</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Jobs</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Subscriptions</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Talent Workshops</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>CPSC Certificates</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Ratings</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Shop Help</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>Contact Us</a>
-                </li>
+
+                { dcLinks.map(link => (
+                    <li key={ link.id }>
+                      <a href={ link.href } className="nav-link">
+                        { link.title }
+                      </a>
+                    </li>
+                  )) }
+
               </ul>
             </div>
 
             <div className="col">
               <h3>SITES</h3>
               <ul className="list-unstyled">
-                <li>
-                  <a href="#" className='nav-link'>DC</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>MAD Magazine</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>DC Kids</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>DC Universe</a>
-                </li>
-                <li>
-                  <a href="#" className='nav-link'>DC Power Visa</a>
-                </li>
+                
+                { sitesLinks.map(link => (
+                  <li key={link.id}>
+                    <a href={link.href} className="nav-link">
+                      {link.title}
+                    </a>
+                  </li>
+                ))}
+                
               </ul>
             </div>
 
             <div className="col d-lg-flex justify-content-end">
-            <img src={dcLogoFooter} alt="DC Logo"/>
-          </div>
+              <img src={ dcLogoFooter } alt="DC Logo" />
+            </div>
 
           </div>
         </div>
